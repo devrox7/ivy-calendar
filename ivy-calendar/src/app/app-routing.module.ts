53ea26eb82674752ab2357c886ce2aa2e129ddbb
Roxana Ion <ionroxana95@yahoo.com>
2019-10-27 21:02:49 +0100

    calendar in progress

--- ivy-calendar/src/app/app-routing.module.ts
+++ ivy-calendar/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+
+  { path: 'home', loadChildren: () => import('../modules/home/home.module').then(m => m.HomeModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ivy-calendar/src/app/app.module.ts
+++ ivy-calendar/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from 'src/layout/main-nav/main-nav.component';
+import { MaterialModule } from 'src/modules/material/material.module';
+import { HomeModule } from 'src/modules/home/home.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MainDashComponent } from 'src/layout/main-dash/main-dash.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MainDashComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
