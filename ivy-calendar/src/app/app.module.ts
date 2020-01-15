3e4756a7e56a394085bdf1b10aed88278569da01
Roxana Ion <roxa0188@edu.eal.dk>
2020-01-15 10:38:28 +0100

    added authentication frontend

--- ivy-calendar/src/app/app.module.ts
+++ ivy-calendar/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from 'src/layout/main-nav/main-nav.component';
 import { MaterialModule } from 'src/modules/material/material.module';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MainDashComponent } from 'src/layout/main-dash/main-dash.component';
 import { AuthenticationModule } from 'src/modules/authentication/authentication.module';
 import { DashboardModule } from 'src/modules/dashboard/dashboard.module';
@@ -17,6 +17,11 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { BasicAuthInterceptor } from 'src/helpers/basic-auth-.interceptor';
+import { ErrorInterceptor } from 'src/helpers/error.interceptor';
+import { fakeBackendProvider } from 'src/helpers/fake-backend';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
@@ -31,12 +36,14 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     AuthenticationModule,
     PerfectScrollbarModule,
     DashboardModule,
+    MatSidenavModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
@@ -44,8 +51,15 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   ],
   providers: [{
     provide: PERFECT_SCROLLBAR_CONFIG,
-    useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-  }],
+    useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+    
+  },
+  { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+  // provider used to create fake backend
+  fakeBackendProvider
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports:[PerfectScrollbarModule]
